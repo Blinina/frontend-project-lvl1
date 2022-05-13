3b74d3b465e28ad999c7790d906a0925ecce9ded
blinina <anastasia.paancake@gmail.com>
2022-05-13 23:28:58 +0300

    hello username

---
+++  src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync';
+const username = readlineSync.question('May I have your name? ');
+export {username};
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import {username} from '../src/cli.js'; 
 console.log("Welcome to the Brain Games!");
+console.log('Hello, '+ username+'!');
