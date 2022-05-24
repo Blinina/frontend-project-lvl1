33095914bc0411413d540a17ffc18fe5aa28d8f8
blinina <anastasia.paancake@gmail.com>
2022-05-24 23:27:26 +0300

    new review

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,30 +2,31 @@ import _ from 'lodash';
 import runGame from '../index.js';
 import generateNumber from '../generateNumber.js';
 
+const searchGcd = (number1, number2) => {
+  const divisors1 = [];
+  const divisors2 = [];
+  for (let i = 1; i <= number1; i += 1) {
+    if (number1 % i === 0) {
+      divisors1.push(i);
+    }
+  }
+  for (let j = 1; j <= number2; j += 1) {
+    if (number2 % j === 0) {
+      divisors2.push(j);
+    }
+  }
+
+  const result = _.intersection(divisors1, divisors2);
+  const generalGcd = result[result.length - 1];
+  return generalGcd;
+};
+
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const startRound = () => {
   const num1 = generateNumber(1, 20);
   const num2 = generateNumber(1, 20);
   const question = `${num1} ${num2}`;
-  const searchGcd = (number1, number2) => {
-    const divisors1 = [];
-    const divisors2 = [];
-    for (let i = 1; i <= number1; i += 1) {
-      if (num1 % i === 0) {
-        divisors1.push(i);
-      }
-    }
-    for (let j = 1; j <= number2; j += 1) {
-      if (num2 % j === 0) {
-        divisors2.push(j);
-      }
-    }
-
-    const result = _.intersection(divisors1, divisors2);
-    const generalGcd = result[result.length - 1];
-    return generalGcd;
-  };
   const correctAnswer = String(searchGcd(num1, num2));
   return [question, correctAnswer];
 };
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,20 +1,21 @@
 import runGame from '../index.js';
 import generateNumber from '../generateNumber.js';
 
+const isPrime = (number) => {
+  const result = [];
+  for (let i = 0; i <= number; i += 1) {
+    if (number % i === 0) {
+      result.push(i);
+    }
+  }
+  const primeAnswer = (result.length === 2);
+  return primeAnswer;
+};
+
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const startRound = () => {
   const question = generateNumber(1, 100);
-  const result = [];
-  const isPrime = (number) => {
-    for (let i = 0; i <= number; i += 1) {
-      if (number % i === 0) {
-        result.push(i);
-      }
-    }
-    const primeAnswer = (result.length === 2);
-    return primeAnswer;
-  };
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
